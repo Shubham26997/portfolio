a1d6c0c82a701c5f65d82813e7fc1b7ba7b2b9eb
Shubham26997 <shubhamgoel386@gmail.com>
2026-03-30 00:21:13 +0530

    Updates

--- assets/Navbar-C4GxKred.js
+++ assets/Navbar-C4GxKred.js
@@ -1,4 +1,4 @@
-import{j as S,r as Be}from"./index-CrXWuzN0.js";import{g as Ae,S as xe}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j as S,r as Be}from"./index-Cg-ZaJIC.js";import{g as Ae,S as xe}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.12.7
  * https://gsap.com
  * 
